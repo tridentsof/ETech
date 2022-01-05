fcdfe5895332eeeb0e2c6d8e9a3dd535eb5cafc2
TienDat174 <nguyentiendat.tk12@gmail.com>
2022-01-05 23:59:32 +0700

    Dat fix chitiepsp

--- ETech.WebApp/KH/TrangChiTietSanPham.aspx.cs
+++ ETech.WebApp/KH/TrangChiTietSanPham.aspx.cs
@@ -25,15 +25,18 @@ namespace ETech.WebApp.KH
                 SqlCommand cmd = new SqlCommand("PROC_VIEW_PRODUCT_DETAIL", dataAccess.getConnection());
                 cmd.CommandType = CommandType.StoredProcedure;
                 cmd.Parameters.AddWithValue("@IDSP",idSP);
-                SqlDataReader dr = cmd.ExecuteReader();
-                while(dr.Read())
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                if (dt != null && dt.Rows.Count>0)
                 {
-                    lblTenSP.Text = dr.GetValue(1).ToString();
-                    lblprice1.Text = String.Format("{0:N0}", dr.GetValue(2)) + "VND";
-                    lblprice2.Text = String.Format("{0:N0}", dr.GetValue(2)) + "VND";
-                    lblNCC.Text = dr.GetValue(3).ToString();
-                    lblCT.Text = dr.GetValue(4).ToString();
-                    lblSL.Text = dr.GetValue(5).ToString();
+                    imgSP.ImageUrl = "~/wwwroot/img/sp/" + dt.Rows[0]["HINHANH"].ToString();
+                    lblTenSP.Text = dt.Rows[0]["TENSANPHAM"].ToString();
+                    lblprice1.Text = String.Format("{0:N0}", dt.Rows[0]["DONGIA"]) + "VND";
+                    lblprice2.Text = String.Format("{0:N0}", dt.Rows[0]["DONGIA"] )+ "VND";
+                    lblNCC.Text = dt.Rows[0]["TENNHACUNGCAP"].ToString();
+                    lblCT.Text = dt.Rows[0]["CHITIET"].ToString();
+                    lblSL.Text = dt.Rows[0]["SOLUONGCONLAI"].ToString();
                 }
                 DataTable cart = new DataTable();
                 if (Session["cart"] == null)
--- ETech.WebApp/KH/TrangChiTietSanPham.aspx.designer.cs
+++ ETech.WebApp/KH/TrangChiTietSanPham.aspx.designer.cs
@@ -96,6 +96,15 @@ namespace ETech.WebApp.KH
         protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator1;
 
         /// <summary>
+        /// CompareValidator1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CompareValidator CompareValidator1;
+
+        /// <summary>
         /// btnBuy control.
         /// </summary>
         /// <remarks>
