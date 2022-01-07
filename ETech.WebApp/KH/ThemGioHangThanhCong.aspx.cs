ba75de588e1f8b04f6233f5c3220f7bcdb4a2f51
tridentsof <gahanhthao@gmail.com>
2022-01-08 00:14:30 +0700

    Fix Bug Cuc Manh v2

--- ETech.WebApp/KH/ThemGioHangThanhCong.aspx.cs
+++ ETech.WebApp/KH/ThemGioHangThanhCong.aspx.cs
@@ -1,12 +1,9 @@
 ﻿using ETech.WebApp.AppCode;
 using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
 using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace ETech.WebApp.KH
 {
